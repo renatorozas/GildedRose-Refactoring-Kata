f53bd2d3d541cebd6d7e6c1ea88c48fae81b7a34
Renato Rozas <imexto@gmail.com>
2024-04-10 06:49:10 -0700

    Add tests to existing behaviour
    Tests should cover all the requirements listed [here](https://github.com/emilybache/GildedRose-Refactoring-Kata/blob/main/GildedRoseRequirements.md).

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -3,9 +3,77 @@ require 'rspec'
 require File.join(File.dirname(__FILE__), 'gilded_rose')
 
 describe GildedRose do
-  it "does not change the name" do
-    items = [Item.new("foo", 0, 0)]
-    GildedRose.new(items).update_quality()
-    expect(items[0].name).to eq "fixme"
+  describe "#update_quality" do
+    it "decreases quality twice as fast once sell_in is 0" do
+      items = [Item.new("foo", 0, 25)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 23
+    end
+
+    it "does not decrease quality under 0" do
+      items = [Item.new("foo", 20, 0)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 0
+    end
+
+    describe "Aged Brie" do
+      it "increases in quality the older it gets" do
+        items = [Item.new("Aged Brie", 20, 25)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 26
+      end
+
+      it "does not increase quality beyond 50" do
+        items = [Item.new("Aged Brie", 20, 50)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 50
+      end
+    end
+
+    describe "Sulfuras, Hand of Ragnaros" do
+      it "does not decrease quality" do
+        items = [Item.new("Sulfuras, Hand of Ragnaros", 0, 80)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 80
+      end
+    end
+
+    describe "Backstage passes to a TAFKAL80ETC concert" do
+      describe "when sell_in is <= 10" do
+        it "increases quality by 2" do
+          items = []
+          (6..10).to_a.each do |n|
+            items << Item.new("Backstage passes to a TAFKAL80ETC concert", n, 25)
+          end
+          GildedRose.new(items).update_quality()
+
+          items.each do |item|
+            expect(item.quality).to eq 27
+          end
+        end
+      end
+
+      describe "when sell_in is <= 5" do
+        it "increases quality by 3" do
+          items = []
+          (1..5).to_a.each do |n|
+            items << Item.new("Backstage passes to a TAFKAL80ETC concert", n, 25)
+          end
+          GildedRose.new(items).update_quality()
+
+          items.each do |item|
+            expect(item.quality).to eq 28
+          end
+        end
+      end
+
+      describe "when sell_in is 0" do
+        it "decreases quality to 0" do
+          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 25)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 0
+        end
+      end
+    end
   end
 end
